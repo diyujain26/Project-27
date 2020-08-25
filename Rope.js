cd0008869df9905719df7fde29f6db9239cddcd3
diyujain26 <67596731+diyujain26@users.noreply.github.com>
2020-08-25 06:27:21 -0700

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,35 @@
+class Rope {
+    constructor(body1,body2,offsetX,offsetY){
+        this.offsetX=offsetX
+        this.offsetY=offsetY
+        var options = {
+        bodyA:body1,
+        bodyB:body2,
+        //stiffness:0.04,
+        //length:10
+        pointB:{x:this.offsetX,y:this.offsetY}
+        }
+       // this.chain=Constraint.create(options)
+        //World.add(world,this.chain);
+        this.rope=Constraint.create(options)
+        World.add(world,this.rope)
+    }
+    display(){
+        var pointA=this.rope.bodyA.position;
+        var pointB=this.rope.bodyB.position;
+       
+        strokeWeight(5)
+
+        stroke("red")
+        fill(0)
+
+        var Anchor1X=pointA.x
+        var Anchor1Y=pointB.y
+
+        var Anchor2X=pointB.x+this.offsetX
+        var Anchor2Y=pointB.y+this.offsetY
+
+        line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+    }
+    
+}
